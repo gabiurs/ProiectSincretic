028610d7a3170dbfbe35b1995d2f12ecc025e4c5
Vetan Denis <94072502+AkiSadako@users.noreply.github.com>
2022-05-16 16:32:46 +0300

    Varianta de Powershell

---
+++ sincretic.ps1
@@ -0,0 +1,14 @@
+﻿#Main
+#Write-Output "Tarile disponibile: "
+#Write-Output "Culoare random: " 
+#$culori | Get-Random -Count
+
+#Intro
+$scriptDir = if (-not $PSScriptRoot) {Split-Path -Parent (Convert-Path ([environment]::GetCommandLineArgs()[0]))} else {$PSScriptRoot}
+$array1 = Get-Content $scriptDir\tari.txt | Sort-Object {Get-Random}
+$array2 = Get-Content $scriptDir\culori.txt | Sort-Object {Get-Random}
+
+
+ForEach ($line in $array1){
+    "{0} - {1}" -f $line, $array2[$array1.IndexOf($line)]
+}
